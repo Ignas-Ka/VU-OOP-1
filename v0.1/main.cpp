cc792f5701e53317a2812d33505096278b180812
ignas <iignas61@gmail.com>
2022-02-14 16:23:04 +0200

    Veikiantis kodas isskyrus random reiksmes pazymiu

--- v0.1/main.cpp
+++ v0.1/main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <algorithm>
+#include <cmath>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::setw;
+using std::setprecision;
+using std::swap;
 
+const int MAX_MOKINIU = 10;
+const int PAZYMIU_SK = 4;
 
 struct data
 {
     string vardas, pavarde;
-    int paz[3] = {0}, egz;
+    int paz[PAZYMIU_SK] = {0}, egz;
     double vid;
 };
 
@@ -20,14 +26,39 @@ void ivestis(data& temp);
 
 void isvestis(data& temp);
 
+bool isNumber(const string& str);
+
+void bubbleSort(int arr[]);
+
+double findMedian(int a[]);
+
 int main()
 {
-    data* duomenys = new data[2];
-    for(data* a = duomenys; a < duomenys + 2; a++) {
+    string arSustabdyti = "0";
+    int index = 0;
+    data* duomenys = new data[MAX_MOKINIU];
+
+    for(data* a = duomenys; a < duomenys + MAX_MOKINIU; a++)
+    {
         ivestis(*a);
+        bubbleSort(&*a -> paz);
+        cout << "Ar bus dar vienas mokinys? (0 - nebus /1 - bus )";
+        cin >> arSustabdyti;
+        if (arSustabdyti != "1")
+        {
+            break;
+        }
+        index++;
+
     }
 
-    for(data* a = duomenys; a < duomenys + 2; a++) {
+//    for(int i = 0; i < PAZYMIU_SK; i++) {
+//        cout << duomenys -> paz[i] << " ";
+//    }
+//    cout << endl;
+
+    for(data* a = duomenys; a < duomenys + index + 1; a++)
+    {
         isvestis(*a);
     }
 
@@ -36,36 +67,87 @@ int main()
     return 0;
 }
 
+bool isNumber(const string& str)
+{
+    return !str.empty() &&
+           std::find_if(str.begin(), str.end(),
+                        [](unsigned char c)
+    {
+        return !std::isdigit(c);
+    }) == str.end();
+}
 
 void ivestis(data& temp)
 {
-    int vidurkis = 0;
+    string egzas = "0";
+    double vidurkis = 0;
+    string pazim = "0";
 
-    int pazymiuSk = 3;
     cout << "Iveskite varda ";
     cin >> temp.vardas;
     cout << "Iveskite pavarde ";
     cin >> temp.pavarde;
-    for(int i = 0; i < pazymiuSk; i++)
+    for(int i = 0; i < PAZYMIU_SK; i++)
     {
-        cout << "Iveskite " << i+1 << " -a(-i) pazymi: ";
-        cin >> temp.paz[i];
+        while(!(isNumber(pazim)) || stoi(pazim) < 1 || stoi(pazim) > 10)
+        {
+            cout << "Iveskite " << i+1 << " -a(-i) pazymi: ";
+            cin >> pazim;
+        }
+        temp.paz[i] = stoi(pazim);
         vidurkis += temp.paz[i];
+        pazim = "0";
     }
-    temp.vid = vidurkis/pazymiuSk;
+    temp.vid = vidurkis/PAZYMIU_SK;
     vidurkis = 0;
-    cout << "Iveskite egzamino ivertinima ";
-    cin >> temp.egz;
+    while(!(isNumber(egzas)) || stoi(egzas) < 1 || stoi(egzas) > 10)
+    {
+        cout << "Iveskite tinkama egzamino ivertinima ";
+        cin >> egzas;
+    }
+    temp.egz = stoi(egzas);
 
 
 }
 
 void isvestis(data& temp)
 {
+    int medArVid = 0;
+
     cout << setw(20) << temp.pavarde << setw(20) << temp.vardas;
-         for(int i = 0; i < 3; i++)
+    for(int i = 0; i < PAZYMIU_SK; i++)
     {
-        cout << setw(10) << temp.paz[i];
+        cout << setw(5) << temp.paz[i];
     }
-    cout << setw(10) << temp.egz << endl;
+    cout << setw(10) << temp.egz;
+    cout << endl;
+    cout << "Mediana(0) ar vidurkis(1) ";
+    cin >> medArVid;
+    if (medArVid == 1) {
+        cout << setw(10) << setprecision(3) << temp.vid << endl;
+    }
+    else {
+        cout << setw(10) << setprecision(3) << findMedian(temp.paz) << endl;
+    }
+}
+
+void bubbleSort(int arr[])
+{
+    for (int i = 0; i < PAZYMIU_SK-1; i++)
+    {
+        for (int j = 0; j < PAZYMIU_SK-i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+                swap(arr[j], arr[j+1]);
+        }
+    }
+}
+
+double findMedian(int a[])
+{
+    if (PAZYMIU_SK % 2 != 0) {
+        return (double)a[PAZYMIU_SK / 2];
+    }
+
+    return (double)(a[(PAZYMIU_SK - 1) / 2] + a[PAZYMIU_SK / 2]) / 2.0;
 }
